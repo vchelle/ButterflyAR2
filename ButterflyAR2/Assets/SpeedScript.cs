dd2eaf28b2fe19f1560e06d90e3defd7a8e660c7
Vchelle <michellepdodson@gmail.com>
2018-03-21 12:52:59 -0400

    Added waypoints
    Added waypoints

---
+++ ButterflyAR2/Assets/SpeedScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedScript : MonoBehaviour
+{
+    public Animation anim;
+
+    void Start()
+    {
+
+        // Walk at double speed
+        anim["butterflyYellow4"].speed = 3.0f;
+    }
+}
